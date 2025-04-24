c2d56c75ab08ae4a7a24fcd4fa9e20bdb9ef38d3
wiyco <72733890+wiyco@users.noreply.github.com>
2025-04-24 19:16:34 +0900

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+// Create an MCP server
+const server = new McpServer({
+  name: "example-server",
+  version: "0.0.1",
+});
+
+// Add an addition tool
+server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
+  content: [{ type: "text", text: String(a + b) }],
+}));
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
